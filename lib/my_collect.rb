3145abc5f55929bd2a0d2c5dfbef321a0f575851
Kim Pham <pham.kim.a@gmail.com>
2018-08-22 22:37:16 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,13 +1,13 @@
 def my_collect(array)
-  i = 0
-  array2 = []
+    i = 0
+    array2 = []
+    
+    while i < array.length
+      array2.push(yield array[i])
+      i += 1
+    end
   
-  while i < array.length
-    array2.push(yield array[i])
-    i += 1
-  end
-  
-  array2
+    array2
 end
 
 students = ["Tim Jones", "Tom Smith", "Jim Campagno"]
